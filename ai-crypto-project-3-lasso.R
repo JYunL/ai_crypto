b399e1a8a1a29d9e0f07a35eb506bb47c53519f6
JYunL <nov27yun@naver.com>
2024-06-11 17:09:26 +0900

    submit project3

---
+++ ai-crypto-project-3-lasso.R
@@ -0,0 +1,79 @@
+library('stringr')
+library('glmnet')
+
+extract <- function(o, s) { 
+  index <- which(coef(o, s) != 0) 
+  data.frame(name=rownames(coef(o))[index], coef=coef(o, s)[index]) 
+}
+
+options(scipen=999)
+
+args<-commandArgs(TRUE)
+
+#args[1] s time
+#args[2] e time
+#args[3] exchange
+#args[4] 
+
+filtered = paste(args[1],args[2],args[3],args[4],'filtered-5-2',args[5],sep="-")
+model_file = paste(args[2],args[3],args[4],args[5],'lasso-5s-2std',sep='-')
+
+#return_file
+filtered <- str_remove_all(filtered,":")
+model_file <- str_remove_all(model_file,":")
+
+filtered = paste ("./", filtered, ".csv", sep="")
+message(filtered)
+message(model_file)
+model_file = paste ("./", model_file, ".csv", sep="")
+
+filtered = read.csv(filtered)
+mid_std = sd(filtered$mid_price)
+message (round(mid_std,0))
+#print (round(mid_std[1],0))
+#print (mid_std)
+
+filtered_no_time_mid = subset(filtered, select=-c(mid_price,timestamp))
+
+y = filtered_no_time_mid$return
+x = subset(filtered_no_time_mid, select=-c(return))
+
+#quit()
+
+x<-as.matrix(x)
+#model_ols<-lm(y~x)
+#model_lasso<-glmnet(x,y)
+
+#cv_fit <- cv.glmnet(x=x, y=y, alpha=0, intercept=FALSE, lower.limits=0, nfolds=10) #ridge
+cv_fit <- cv.glmnet(x=x, y=y, alpha=1, intercept=FALSE, lower.limits=0, nfolds=5) #lasso
+
+fit <- glmnet(x=x, y=y, alpha = 1, lambda=cv_fit$lambda.1se, intercept=FALSE, lower.limits=0,)
+#coef(fit)
+
+df <- extract(fit, s=0.1)
+df <- t(df)
+write.table(df, file=model_file, sep=",", col.names=FALSE, row.names=FALSE, quote=FALSE)
+
+
+# PnL 계산
+pnl_data <- read.csv("./ai-crypto-project-3-live-btc-krw.csv")
+
+pnl_data$PnL <- ifelse(pnl_data$side == "buy", 
+                       pnl_data$quantity * (pnl_data$price - lag(pnl_data$price)) - pnl_data$fee, 
+                       pnl_data$quantity * (lag(pnl_data$price) - pnl_data$price) - pnl_data$fee)
+pnl_data$PnL[is.na(pnl_data$PnL)] <- 0
+
+pnl_data$cum_PnL <- cumsum(pnl_data$PnL)
+
+num_trades <- nrow(pnl_data)
+min_pnl <- min(pnl_data$cum_PnL)
+max_pnl <- max(pnl_data$cum_PnL)
+
+# 결과 파일에 저장
+result <- data.frame(
+  total_PnL = sum(pnl_data$PnL),
+  min_pnl = min_pnl,
+  max_pnl = max_pnl
+)
+
+write.table(result, file="./PnL_results.csv", sep=",", col.names=TRUE, row.names=FALSE, quote=FALSE)
